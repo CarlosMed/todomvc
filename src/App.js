001a4ebe23b650bc4763452540dc603e9285f197
Carlos Medina <carlos.w.med@gmail.com>
2019-06-05 23:28:46 -0400

    chore: clean todo scaffold

--- src/App.js
+++ src/App.js
@@ -1,24 +1,75 @@
 import React from 'react';
-import logo from './logo.svg';
+import '../node_modules/todomvc-app-css/index.css';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+      <section className="todoapp">
+        <header className="header">
+          <h1>todos</h1>
+          <input
+            type="input"
+            className="new-todo"
+            placeholder="What needs to be done?"
+          />
+        </header>
+
+        <section className="main">
+          <input id="toggle-all" className="toggle-all" type="checkbox" />
+          <label htmlFor="toggle-all">Mark all as complete</label>
+          <ul className="todo-list">
+            <li className="completed">
+              <div className="view">
+                <input type="checkbox" className="toggle" />
+                <label>test task</label>
+                <button className="destroy" />
+              </div>
+            </li>
+            <li>
+              <div className="view">
+                <input type="checkbox" className="toggle" />
+                <label>test task</label>
+                <button className="destroy" />
+              </div>
+            </li>
+          </ul>
+        </section>
+
+        <footer className="footer">
+          <span className="todo-count">
+            <strong>8</strong> items left
+          </span>
+          <ul className="filters">
+            <li>
+              <a href="#/" className="selected">
+                All
+              </a>
+            </li>
+            <li>
+              <a href="#/active">Active</a>
+            </li>
+            <li>
+              <a href="#/completed">Completed</a>
+            </li>
+          </ul>
+          <button className="clear-completed">Clear completed</button>
+        </footer>
+      </section>
+
+      <footer className="info">
+        <p>Double-click to edit a todo</p>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Created by <a href="http://twitter.com/oscargodson">Oscar Godson</a>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <p>
+          Refactored by
+          <a href="https://github.com/cburgmer">Christoph Burgmer</a>
+        </p>
+        <p>
+          Part of <a href="http://todomvc.com">TodoMVC</a>
+        </p>
+      </footer>
     </div>
   );
 }
